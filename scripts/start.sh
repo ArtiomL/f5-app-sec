b071def19840bf5cba5a039771dcc23d9ed68292
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-11 23:10:25 +0300

    Docker Wrapper Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+# f5-app-sec - Docker Wrapper Script
+# https://github.com/ArtiomL/f5-app-sec
+# Artiom Lichtenstein
+# v1.0.1, 12/09/2018
 
+openssl req -new -x509 -days 365 -nodes -out /etc/apache2/ssl/self.pem -keyout /etc/apache2/ssl/self.key -subj "/C=US/ST=Washington/L=Seattle/O=F5 Networks Inc/CN=f5-app-sec" > /dev/null 2>&1
+
+exec /usr/sbin/apachectl -DFOREGROUND
